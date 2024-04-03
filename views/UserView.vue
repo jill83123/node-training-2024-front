4f28eb09a9c9a3ce17454ba23a879a7a99b661ab
YU-FANG <jill83123@gmail.com>
2024-04-04 01:18:39 +0800

    Updates

--- views/UserView.vue
+++ views/UserView.vue
@@ -1,7 +1,9 @@
 <template>
   <NavComponent />
 
-  <div class="min-h-[calc(100vh_-_66px)]" style="background-image: url('/images/bg.svg')">
+  <div
+    class="min-h-[calc(100vh_-_66px)]"
+    :style="{ 'background-image': `url(${BASE_URL}/images/bg.svg)` }">
     <div class="container flex justify-between gap-7 py-12">
       <main class="w-8/12">
         <RouterView />
@@ -50,7 +52,10 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import NavComponent from '@/components/NavComponent.vue';
+
+const BASE_URL = ref(import.meta.env.BASE_URL);
 </script>
 
 <style lang="postcss">
