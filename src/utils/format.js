f229e672b03323443701864ffb38901330dfc21c
YU-FANG <jill83123@gmail.com>
2024-05-16 01:26:05 +0800

    fix: formatDate 時和分修正為兩位數格式

--- src/utils/format.js
+++ src/utils/format.js
@@ -4,7 +4,7 @@ export function formatDate(timestamp) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const hours = `0${date.getHours()}`.slice(-2);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
   return `${year}/${month < 10 ? `0${month}` : month}/${day < 10 ? `0${day}` : day} ${hours}:${minutes}`;
 }
