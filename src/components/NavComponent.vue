b75cbae99153f046de5beba0b11f47d745930369
YU-FANG <jill83123@gmail.com>
2024-05-23 18:16:40 +0800

    feat: 串接發送驗證信、驗證信箱的 API

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="border-b-[3px] border-primary bg-white py-3">
+  <VueLoading v-model:active="isLoading" />
+
+  <nav class="fixed left-0 right-0 top-0 z-50 border-b-[3px] border-primary bg-white py-3">
     <div class="container flex items-center justify-between">
       <h1 class="font-Paytone-One text-[26px]">
         <RouterLink to="/">MetaWall</RouterLink>
@@ -48,11 +50,24 @@
       </div>
     </div>
   </nav>
+
+  <template v-if="!userStore.user.isVerifiedEmail">
+    <p
+      class="fixed left-0 right-0 top-[66px] z-10 bg-white bg-opacity-75 py-2 text-center font-bold">
+      您目前尚未驗證信箱，僅能瀏覽貼文，請至信箱點擊驗證連結。沒有收到信嗎？
+      <button type="button" class="text-secondary" @click="sendVerificationEmail">點我</button>
+      發送驗證信
+    </p>
+  </template>
+
+  <div class="h-[66px]" :class="{ 'mb-[41px]': !userStore.user.isVerifiedEmail }" />
 </template>
 
 <script setup>
+import axios from 'axios';
 import { ref, onMounted } from 'vue';
 import createUserStore from '@/stores/userStore';
+import { showToast } from '@/utils/sweetAlert';
 
 const userStore = createUserStore();
 
@@ -68,4 +83,17 @@ onMounted(() => {
     }
   });
 });
+
+const isLoading = ref(false);
+
+async function sendVerificationEmail() {
+  isLoading.value = true;
+  try {
+    await axios.post(`${import.meta.env.VITE_API_URL}/user/sendVerificationMail`);
+    showToast({ icon: 'success', title: '發送成功' });
+  } catch (err) {
+    showToast({ icon: 'error', title: err.response?.data?.message || err.message });
+  }
+  isLoading.value = false;
+}
 </script>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,9 @@
 <template>
   <VueLoading v-model:active="isLoading" />
-  <NavComponent />
 
-  <div
-    class="bg relative min-h-[calc(100vh_-_66px)] after:bg-[length:36px_36px] after:sm:bg-[length:64px_64px]">
+  <div class="bg relative min-h-screen after:bg-[length:36px_36px] after:sm:bg-[length:64px_64px]">
+    <NavComponent />
+
     <div class="container flex justify-between gap-7 py-12">
       <main class="w-full lg:w-[62%]">
         <RouterView v-if="isChecked" />
@@ -16,17 +16,31 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 import createUserStore from '@/stores/userStore';
 import NavComponent from '@/components/NavComponent.vue';
 import SecNavComponent from '@/components/SecNavComponent.vue';
+import { showToast } from '@/utils/sweetAlert';
 
+const route = useRoute();
 const router = useRouter();
 const userStore = createUserStore();
 
 const isLoading = ref(false);
 const isChecked = ref(false);
+
+async function verifyMail() {
+  try {
+    const { token } = route.query;
+    await axios.post(`${import.meta.env.VITE_API_URL}/user/verifyMail`, { token });
+    showToast({ icon: 'success', title: '驗證成功' });
+    router.push('/');
+  } catch (err) {
+    showToast({ icon: 'error', title: err.response?.data?.message || err.message });
+  }
+}
+
 onMounted(async () => {
   const token = document.cookie.replace(
     /(?:(?:^|.*;\s*)MetaWallToken\s*=\s*([^;]*).*$)|^.*$/,
@@ -40,11 +54,21 @@ onMounted(async () => {
   }
 
   isLoading.value = true;
+
+  const { token: verificationMailToken } = route.query;
+  if (verificationMailToken) {
+    verifyMail();
+  }
+
   try {
     await axios.post(`${import.meta.env.VITE_API_URL}/user/check`);
     await userStore.getUserProfile();
     isChecked.value = true;
   } catch (err) {
+    if (err.response.status === 429) {
+      showToast({ icon: 'error', title: err.response?.data?.message || err.message });
+      return;
+    }
     router.push('/sign/in');
   }
   isLoading.value = false;
